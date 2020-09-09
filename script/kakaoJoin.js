740d10f638cc06ac454c9dcae06f6aa898ab7345
JaneL <juyeonsh412@gmail.com>
2020-09-09 12:06:21 +0900

    유효성검사_javascript

--- script/kakaoJoin.js
+++ script/kakaoJoin.js
@@ -1,72 +1,75 @@
 //한글자라도 쓰면 x표시 나타나게 하기 시작
+blockXBtn();
+
 function blockXBtn() {
     let numOfChar = 0;
     let xBtn = document.querySelectorAll(".xBtn");
     let selectInput = document.querySelectorAll(".selectInput");
     for (let i = 0; i < selectInput.length; i++) {
-        selectInput[i].addEventListener("keyup", function(){
-            if (this.value.length !== 0)  {
-                xBtn[i].style.display="block";     
-            }
-            else if (this.value.length == 0)  {
-                xBtn[i].style.display="none";     
+        selectInput[i].addEventListener("keyup", function() {
+            if (this.value.length !== 0) {
+                xBtn[i].style.display = "block";
+            } else if (this.value.length == 0) {
+                xBtn[i].style.display = "none";
             }
         })
     }
 }
-blockXBtn();
 //한글자라도 쓰면 x표시 나타나게 하기 끝
 
 //x 누르면 input value값 삭제 시작
+deleteContent();
+
 function deleteContent() {
     let xBtn = document.querySelectorAll(".xBtn");
     let selectInput = document.querySelectorAll(".selectInput");
     for (let i = 0; i < xBtn.length; i++) {
-        xBtn[i].addEventListener("click", function(){
+        xBtn[i].addEventListener("click", function() {
             selectInput[i].value = "";
-            xBtn[i].style.display="none";
+            xBtn[i].style.display = "none";
         })
-    } 
+    }
 }
-deleteContent();
 //x 누르면 input value값 삭제 끝
 
 
 //focus일 때, fieldset border-bottom 색 변경 끝
+ChangeFieldsetBorderColor();
+
 function ChangeFieldsetBorderColor() {
     let selectInput = document.querySelectorAll(".selectInput");
     let fieldSet = document.querySelectorAll(".selectFieldset");
 
     //while (i < 4) {
-        for(let i = 0; i < selectInput.length; i++) {
-            selectInput[i].addEventListener("focus", function(){
-                fieldSet[i].style.borderBottom = "2px solid #666";
-            })
-            selectInput[i].addEventListener("blur", function(){
-                fieldSet[i].style.borderBottom = "";
-            })
-        }
+    for (let i = 0; i < selectInput.length; i++) {
+        selectInput[i].addEventListener("focus", function() {
+            fieldSet[i].style.borderBottom = "2px solid #666";
+        })
+        selectInput[i].addEventListener("blur", function() {
+            fieldSet[i].style.borderBottom = "";
+        })
+    }
     //}
 }
-ChangeFieldsetBorderColor();
 //focus일 때, fieldset border-bottom 색 변경 끝
 
 
 //닉네임 글자수에 따른 CntNumber 변동 시작
+document.querySelector("#uName").addEventListener("keyup", fnLength);
+
 numOfChar = 20;
 document.querySelector("#numOfChar").innerHTML = numOfChar;
 
 function fnLength() {
-    let cntNumOfWords = document.querySelector("#putName").value;
+    let cntNumOfWords = document.querySelector("#uName").value;
     let length = cntNumOfWords.length;
     if (length > numOfChar) {
         cntNumOfWords = cntNumOfWords.substring(0, numOfChar);
-        document.querySelector("#putName").value = cntNumOfWords;
+        document.querySelector("#uName").value = cntNumOfWords;
         length = cntNumOfWords.length;
     }
     document.querySelector("#numOfWord").innerHTML = length;
 }
-document.querySelector("#putName").addEventListener("keyup", fnLength);
 //닉네임 글자수에 따른 CntNumber 변동 끝
 
 
@@ -92,6 +95,7 @@ function fnBirthInit() {
     year.innerHTML = yearOptTag;
 }
 //출생년도 표시 끝
+month.addEventListener("click", fnDelMonthOpt);
 
 function fnDelMonthOpt() {
     var monthDelOpt = document.querySelector("#monthDelOpt");
@@ -101,8 +105,8 @@ function fnDelMonthOpt() {
     }
 }
 
-month.addEventListener("click", fnDelMonthOpt);
 
+day.addEventListener("click", fnDelDateOpt);
 
 function fnDelDateOpt() {
     var dateDelOpt = document.querySelector("#dateDelOpt");
@@ -112,7 +116,7 @@ function fnDelDateOpt() {
     }
 }
 
-day.addEventListener("click", fnDelDateOpt);
+month.addEventListener("change", fnEndDate);
 
 function fnEndDate() {
 
@@ -138,7 +142,6 @@ function fnEndDate() {
     day.innerHTML = dateOptTag;
 }
 
-month.addEventListener("change", fnEndDate);
 
 //출생월 표시 시작
 var monthOptTag = `<option id="monthDelOpt">월</option>`;
@@ -157,6 +160,8 @@ day.innerHTML = dateOptTag;
 //월별 마지막 날짜 표시 끝
 
 //년도 변경시 월, 일 초기화
+year.addEventListener("change", fnReset);
+
 function fnReset() {
     var monthOptTag = `<option id="monthDelOpt">월</option>`;
     for (var i = 1; i <= 12; i++) {
@@ -165,12 +170,61 @@ function fnReset() {
     month.innerHTML = monthOptTag;
 }
 
-year.addEventListener("change", fnReset);
 // 생년월일 옵션(select) 끝
 
+
 //유효성검사 시작
+function submitFrm() {
 
+    let errorMessage = document.querySelectorAll(".errorMessage");
 
+    let uEmail = document.querySelector("#uEmail");
+    let uEmail_val = uEmail.value;
 
+    let uPw = document.querySelector("#uPw");
+    let uPw_val = uPw.value;
+
+    let uPw_Re = document.querySelector("#uPw_Re");
+    let uPw_Re_val = uPw_Re.value;
+
+    let uName = document.querySelector("#uName");
+    let uName_val = uName.value;
+
+    let uNum = document.querySelector("#uNum");
+    let uNum_val = uNum.value;
+
+    if (uEmail_val == "") {
+        errorMessage[0].innerHTML = "카카오계정 이메일을 입력해주세요.";
+        errorMessage[0].style.display = "block";
+        uEmail.focus();
+    } else if (uPw_val == "") {
+        errorMessage[0].style.display = "none";
+        errorMessage[1].innerHTML = "카카오계정 비밀번호를 입력해주세요.(영문자/숫자/특수문자)";
+        errorMessage[1].style.display = "block";
+        uPw.focus();
+    } else if (uPw_Re_val == "") {
+        errorMessage[1].style.display = "none";
+        errorMessage[2].innerHTML = "카카오계정 비밀번호를 입력해주세요.(영문자/숫자/특수문자)";
+        errorMessage[2].style.display = "block";
+        uPw_Re.focus();
+    } else if (uPw_val !== uPw_Re_val) {
+        errorMessage[2].innerHTML = "입력한 비밀번호와 재입력한 비밀번호가 일치하지 않습니다. 다시 확인해 주세요.";
+        uPw_Re.focus();
+    } else if (uName_val == "") {
+        errorMessage[2].style.display = "none";
+        errorMessage[3].innerHTML = "닉네임이 등록되지 않았습니다. 닉네임을 입력해주세요.";
+        errorMessage[3].style.display = "block";
+        uName.focus();
+    } else if (uNum_val == "") {
+        errorMessage[3].style.display = "none";
+        errorMessage[4].innerHTML = "전화번호가 등록되지 않았습니다. 전화번호를 입력해주세요.";
+        errorMessage[4].style.display = "block";
+        uNum.focus();
+    } else {
+        frm.action = "https://daum.net";
+        frm.method = "get";
+        frm.submit();
+    }
+}
 
 //유효성검사 끝
